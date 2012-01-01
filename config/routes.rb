66d5324576d6afa2fd71dcf1ef9c7ac64cb91802
Romain Tribes <tribes.romain@gmail.com>
2012-01-01 23:27:56 +0100

    Use a new route for attacks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 ConquestOnRailsDev::Application.routes.draw do
   root to: 'application#home'
-  
+
   post '/users', to: 'users#create', as: 'users'
-  
+
   post   '/sign_in',  to: 'sessions#create',  as: 'sign_in'
   delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
-  
+
   get '/dashboard', to: 'users#dashboard'
-  
+
   post '/games/find', to: 'games#find', as: 'find_game'
   get  '/games/:id',  to: 'games#show', as: 'game'
-  
+
   scope '/games/:game_id' do
-    post '/ownerships/:id/attack', to: 'ownerships#attack', as: 'ownership_attack'
-    post '/ownerships/:id/deploy', to: 'ownerships#deploy', as: 'ownership_deploy'
+    post '/attacks', to: 'attacks#create', as: 'attacks'
   end
-  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
